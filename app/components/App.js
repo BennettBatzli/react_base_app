e0dd4bb2b4b097f25300b2ef0efacfd9e52ad565
Bennett Batzli <bennett.batzli@gmail.com>
2016-09-22 14:38:56 -0500

    initial commit

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var App = React.createClass({
+	render: function() {
+		return (<div>
+					<h1>React is here!</h1>
+					<p>to stay.</p>
+				</div>);
+	}
+});
+
+module.exports = App;
